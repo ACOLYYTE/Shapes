0a8fb85e7d7b6d95e8666795f103bbb70bbc3d18
ACOLYTE <maksikxx@ukr.net>
2022-07-10 12:13:09 +0300

    quadro first commit

---
+++ src/Shapes/quadro/quadro.java
@@ -0,0 +1,9 @@
+package Shapes.quadro;
+import Shapes.Shapes;
+public class quadro extends Shapes  {}
+class VipucliyQuadro extends quadro{}
+class Paralelogram extends quadro{}
+class StraightAngle extends quadro{}
+class Square extends quadro{}
+class Romb extends quadro{}
+class Trapecia extends quadro{}
---
+++ src/Shapes/triangle/triangle.java
@@ -0,0 +1,6 @@
+package Shapes.triangle;
+import Shapes.Shapes;
+public class triangle extends Shapes {}
+class StraightTriangle extends triangle{}
+class EqualsTriangle extends  triangle{}
+class StraightAngleTriangle extends triangle{}
